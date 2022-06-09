376e1f80a6c5be756f9aa11626946275157b68fd
Arif Puji Setiawan <arif.p.setiawan.9@gmail.com>
2022-06-09 23:06:59 +0700

    Add task

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (reaction) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const theaterAll = [...theaterIXX, ...theaterVGC];
+
+  const filteredReaction = theaterAll.filter(
+    (item) => item.hasil === reaction
+  ).length;
+
+  return filteredReaction;
+};
 
 module.exports = {
   promiseOutput,
